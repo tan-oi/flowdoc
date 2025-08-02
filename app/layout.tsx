4acf63f563abb89e625c0bfc61f60c8e3e562fb8
Tanay <tanayghoriwala001@gmail.com>
2025-08-02 23:48:39 +0530

    chore : analytics + speed insights by vercel added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import Providers from "@/lib/providers";
 
@@ -32,6 +32,8 @@ export default function RootLayout({
       >
         <Providers>
           <main>{children}</main>
+          <SpeedInsights />
+          <Analytics/>
         </Providers>
       </body>
     </html>
